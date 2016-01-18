b9a8def6b12d429f2f0f1330ff7dbef100cf20c2
Nils Bunger <nilsbunger@gmail.com>
2016-01-18 14:54:29 -0800

    add static

--- icebreaker/icebreaker/settings.py
+++ icebreaker/icebreaker/settings.py
@@ -114,6 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+print BASE_DIR
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
