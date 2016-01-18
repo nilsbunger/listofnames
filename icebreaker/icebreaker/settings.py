ecc123cc57845a5eb4455249391102c2ea8587c8
Jack Pien <jack@jackpien.com>
2016-01-18 15:15:27 -0800

    Add main index.html view

--- icebreaker/icebreaker/settings.py
+++ icebreaker/icebreaker/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'listofnames',
+    'listofnames.apps.ListofnamesConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
